48015792a4747fdc0d2b1e56c4f02d96c92c4be4
too-late01 <sysoevalida@gmail.com>
2022-12-26 12:43:23 +0300

    commit

--- src/Homework.java
+++ src/Homework.java
@@ -4,8 +4,8 @@ public class Homework {
     public static void main(String[] args) {
         Scanner sc =new Scanner (System.in);
         System.out.println ("Please enter your full name");
-        String fullName = sc.nextLine ();
-        String encodedFullName = Base64.getEncoder().encodeToString(fullName.getBytes());
+        String name = sc.nextLine ();
+        String encodedFullName = Base64.getEncoder().encodeToString(name.getBytes());
         System.out.println("Program result: " + encodedFullName);
     }
 }
