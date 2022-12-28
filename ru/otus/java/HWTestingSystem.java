b4d73ae242c3d2862b9767f5a84edfa8fc015ad8
too-late01 <sysoevalida@gmail.com>
2022-12-29 01:20:23 +0300

    3 homework 2 try

--- ru/otus/java/HWTestingSystem.java
+++ ru/otus/java/HWTestingSystem.java
@@ -1,42 +1,42 @@
 package ru.otus.java;
 
-import java.lang.reflect.Array;
 import java.util.Scanner;
 public class HWTestingSystem {
     static int correctCount = 0;
     static int wrongCount = 0;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < Questions.questions.length; i++) {
             System.out.println(Questions.questions[i]);
-
-            for (int j = 0; j < Answers.answerToQuestion1.length; j++) {
-                System.out.println(Answers.answerToQuestion1[i][j]);
+            for (int j = 0; j < Answers.answerToQuestions.length; j++) {
+                System.out.println(Answers.answerToQuestions[i][j]);
             }
-            System.out.print("Ваш ответ: ");
-            int answer = scanner.nextInt();
-
-            if (answer == Answers.correctAnswers[i]) {
+            if (Answers.GetAnswer() == Answers.correctAnswers[i]) {
                 System.out.println("Correct answer");
                 correctCount = correctCount + 1;
             } else {
                 System.out.println("Wrong answer");
                 wrongCount = wrongCount + 1;
             }
-    }
+        }
         System.out.println("Результат: правильно " + correctCount + ", неправильно " + wrongCount);
-}
+    }
     class Questions {
-        static String[] questions ={"В файл с каким расширением компилируется java-файл?",
+        static String[] questions = {"В файл с каким расширением компилируется java-файл?",
                 "С помощью какой команды git можно получить полную копию удаленного репозитория?",
                 "Какой применяется цикл, когда не известно количество итераций?"};
-        }
-    class Answers {
-        static String[][] answerToQuestion1 ={
+    }
+    static class Answers {
+        static String[][] answerToQuestions = {
                 {"1. cs", "2. java", "3. class", "4. exe"},
                 {"1. commit", "2. push", "3. clone", "4. copy"},
                 {"1. while", "2. for", "3. loop"}
         };
         static int[] correctAnswers = {2, 3, 1};
+        public static int GetAnswer() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Ваш ответ: ");
+            int answer = scanner.nextInt();
+            return answer;
         }
     }
+}
