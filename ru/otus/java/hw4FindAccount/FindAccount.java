70a4c44f3582b7936d53ad4616636e14fb578b15
too-late01 <sysoevalida@gmail.com>
2023-02-02 16:10:11 +0300

    по комментариям

--- ru/otus/java/hw4FindAccount/FindAccount.java
+++ ru/otus/java/hw4FindAccount/FindAccount.java
@@ -3,136 +3,6 @@ package ru.otus.java.hw4FindAccount;
 // интерфейсы в методах (последний пример с урока по разбору кода)??
 
 import java.util.*;
-
-class BankSet {
-    private Map<Client, List<ClientAccount>> clients = new HashMap<>(); // у вас было написано private final,
-    // но через константу переменная класса не создавалась
-    private Map<ClientAccount, Client> accounts = new HashMap<>();
-
-
-    public BankSet(Map<Client, List<ClientAccount>> clients, Map<ClientAccount, Client> accounts) {
-        this.clients = clients;
-        this.accounts = accounts;
-    }
-    public List <ClientAccount> getAccounts (Client client) {
-        return client.getClientAccounts();
-    }
-    public Client findClient(ClientAccount accountNumber) {
-        Client showClient = null;
-            System.out.println("Поиск клиента по номеру счета(use HashMap): "+accounts.get(accountNumber));
-        return showClient;
-    }
-    public void addClientAccount(Client client, ClientAccount account) {
-        accounts.put(account, client);
-        List <ClientAccount> clientAccountAddition = clients.get(client);
-        clientAccountAddition.add(account);
-        clients.put(client, clientAccountAddition);
-    }
-}
-
-class Bank {
-    private ArrayList<Client> clients;
-    private int i;
-    public Bank (ArrayList<Client> clients) {
-        this.clients = clients;
-    }
-
-    public List <ClientAccount> getAccounts (Client client) { // мы должны как-то получить элемент класса Клиента
-       return client.getClientAccounts(); // не нужно создавать отдельный массив и перебором закидывать в него счета, если в класе Клиент счета уже хранятся в массиве. Просто вытаскивать из клааса этот элементомассив
-    }
-
-    public Client findClient(ClientAccount accountNumber) {
-       Client showClient = null;
-        for (int i = 0; i < clients.size(); i++) {
-           // ArrayList<ClientAccount> listAccount = clients.get(i).checkClientAccount();
-            List<ClientAccount> listAccount = clients.get(i).getClientAccounts(); /*переделала с использованием этого метода.
-            пока не сравнила, что они практически идентичны, не обращала внимания. */
-            for (int j = 0; j < clients.get(i).getNumOfAccounts(); j++) {
-                ClientAccount currentAccount = listAccount.get(j);
-                if (accountNumber.equals(currentAccount)) {
-                   showClient = clients.get(i);
-                   //System.out.println(clients.get(i));
-                   return showClient;
-                }
-              else showClient = null;
-
-            }
-        }
-        return showClient;
-    }
-}
-
-class Client {
-    private String name;
-    private int age;
-    private List<ClientAccount> clientAccounts;
-
-    public Client (String name, int age, ArrayList<ClientAccount> clientAccounts) {
-        this.age=age;
-        this.name = name;
-        this.clientAccounts = clientAccounts;
-    }
-    public String getName() {
-        return this.name;
-    }
-    public List <ClientAccount> getClientAccounts() {
-        return this.clientAccounts;
-    }
-    public void addAccount(ClientAccount ac){
-        clientAccounts.add(ac);
-    }
-    public int getNumOfAccounts() {
-        return clientAccounts.size();
-    }
-    public ClientAccount getAccount(int i) {
-        return clientAccounts.get(i);
-    }
-
-    @Override
-    public String  toString () {
-        return "Name "+ name + " "+"age:"+ String.valueOf(age); //+" "+ clientAccounts.toString();
-    }
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, age, clientAccounts);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Client client = (Client) o;
-        return age == client.age && Objects.equals(name, client.name) && Objects.equals(clientAccounts, client.clientAccounts);
-    }
-}
-
-class ClientAccount {
-    private int money;
-    private int accountNumber;
-    public ClientAccount(int accountNumber, int money) {
-        this.money=money;
-        this.accountNumber=accountNumber;
-    }
-
-    @Override
-    public String  toString () {
-        return ("account:"+String.valueOf(accountNumber)+" money:"+ String.valueOf(money));
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ClientAccount that = (ClientAccount) o;
-        return money == that.money && accountNumber == that.accountNumber;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(money, accountNumber);
-    }
-}
-
 public class FindAccount {
     public static void main (String[] args ) {
 
@@ -177,6 +47,16 @@ public class FindAccount {
         System.out.println("Счета 1 клиента (use HashMap): "+client1accountsBankList);
 
         var accountofClient1BankList = bankList.findClient(new ClientAccount(4,30));//Max)
+
+        System.out.println("Добавление счета к существующему клиенту");
+        bankList.addClientAccount(client1, new ClientAccount(6, 500));
+        System.out.println(bankList);
+
+        System.out.println("Добавление счета к несуществующему клиенту");
+        ArrayList<ClientAccount> client3Account = new ArrayList<>();
+        Client client3 = new Client("Alma", 25,client3Account);
+        bankList.addClientAccount(client3, new ClientAccount(7, 50));
+        System.out.println(bankList);
 
 
     }
